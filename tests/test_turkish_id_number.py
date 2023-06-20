bf6d4776df02d54781c2cf6cd91310fad65a4b00
Hasan Ozdemir <hasanozdemir1@trakya.edu.tr>
2023-06-20 21:43:07 +0300

    feature: project restructured

--- tests/test_turkish_id_number.py
+++ tests/test_turkish_id_number.py
@@ -1,9 +1,9 @@
-import turkish_validator as tv
-
+import src.turkish_validator as tv
 # invalid input
 def test_turkish_id_invalid():
-    assert True==True
+    assert True == True
+
 
 # valid input
 def test_turkish_id_valid():
-    assert True==True
+    assert True == True
